f14f0f654424e2bfa0732093d8b3a6c695be0c7f
yasutani_naoto <yasutani_naoto@cyberagent.co.jp>
2023-08-26 21:46:49 +0900

    Add comment.

--- restrun/generator/__init__.py
+++ restrun/generator/__init__.py
@@ -13,6 +13,8 @@ AUTO_GENERATED_COMMENT = re.compile(
 def is_auto_generated(source: Path | str) -> bool:
     if isinstance(source, Path):
         with open(source, "r") as file:
+            # NOTE: Considering that comments are placed at the top of the file,
+            #       it is not necessary to read that many characters.
             code = file.read(200)
     else:
         code = source
