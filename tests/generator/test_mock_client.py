58c76dc70b3aa212e2c08c717932c2c85517ddb2
yasutani_naoto <yasutani_naoto@cyberagent.co.jp>
2023-08-26 18:19:04 +0900

    Fix test.

--- tests/generator/test_mock_client.py
+++ tests/generator/test_mock_client.py
@@ -1,3 +1,9 @@
+import os
+from pathlib import Path
+
+from restrun.config import Config
+from restrun.config.v1 import V1Config
+from restrun.generator.client import ClientGenerator
 from restrun.generator.context import Context
 from restrun.generator.mock_client import MockClientGenerator
 
@@ -5,8 +11,23 @@ from restrun.generator.mock_client import MockClientGenerator
 class TestMockClientGenerator:
     def test_generate(self):
         locals = {}
-        code = MockClientGenerator().generate(Context(client_prefix="My"))
 
-        exec("class MyClient: pass\n" + code, None, locals)
+        context = Context.from_config(
+            Config(
+                root=V1Config(
+                    name="my",
+                    version="1",
+                )
+            )
+        )
+        files = [Path(__file__).parent / "client.py"]
+        for file in files:
+            with open(Path(__file__).parent / "client.py", "w") as file:
+                file.write(ClientGenerator().generate(context))
+
+        exec(MockClientGenerator().generate(context), None, locals)
+
+        for file in files:
+            os.remove(file)
 
         assert locals["MyMockClient"] is not None
