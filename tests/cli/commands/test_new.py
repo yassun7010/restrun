898774dcc37517233d8ed3d8a1351fbb37a45a62
yassun7010 <yassun7010@outlook.com>
2023-09-23 01:35:46 +0900

    Add: new command test.

---
+++ tests/cli/commands/test_new.py
@@ -0,0 +1,15 @@
+import os
+from restrun.cli.app import App
+
+
+class TestCliAppNewCommand:
+    def test_new_command(self) -> None:
+        os.chdir("examples")
+        App.run(
+            [
+                "new",
+                "new_sample",
+                "--openapi",
+                "https://petstore3.swagger.io/api/v3/openapi.json",
+            ]
+        )
