c33433fbced499657702bc1cb8a773baebeeaabc
yassun7010 <yassun7010@outlook.com>
2023-09-23 22:34:47 +0900

    Add: test case.

---
+++ tests/cli/commands/get/test_config.py
@@ -0,0 +1,14 @@
+from restrun.cli.app import App
+from tests.data import DATA_DIR
+
+
+class TestCliAppGetConfigCommand:
+    def test_get_config(self) -> None:
+        App.run(
+            [
+                "--config",
+                str(DATA_DIR / "restrun.yml"),
+                "get",
+                "config",
+            ]
+        )
