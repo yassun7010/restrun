0114339c9c1956daf558319d8647fc1fb23ae1e2
yassun7010 <yassun7010@outlook.com>
2023-09-25 01:47:41 +0900

    Update: test case.

---
+++ tests/generator/test_schemas_module.py
@@ -0,0 +1,18 @@
+from restrun.config import Config
+from restrun.generator import is_auto_generated_or_empty
+from restrun.generator.context.restrun_context import RestrunContext
+from restrun.generator.schemas_module import SchemasModuleGenerator
+
+
+class TestSchemasModuleGenerator:
+    def test_check_auto_generated(
+        self,
+        config: Config,
+        restrun_context: RestrunContext,
+    ) -> None:
+        assert is_auto_generated_or_empty(
+            SchemasModuleGenerator().generate(
+                config,
+                restrun_context,
+            )
+        )
