5ddc7fd072b1c46e7ed67928c8a52f48ba1834a7
yassun7010 <yassun7010@outlook.com>
2023-09-25 01:42:23 +0900

    Add: test case.

--- tests/cli/commands/create/test_config.py
+++ tests/cli/commands/create/test_config.py
@@ -2,11 +2,17 @@ import contextlib
 
 from tempfile import TemporaryDirectory
 
+import pytest
+
 from restrun.cli.app import App
 from tests.data import DATA_DIR
 
 
 class TestCliAppCreateConfigCommand:
+    def test_create_config_command_help(self) -> None:
+        with pytest.raises(SystemExit):
+            App.run(["create", "config", "--help"])
+
     def test_create_config_command(self) -> None:
         with TemporaryDirectory() as dir:
             with contextlib.chdir(dir):
