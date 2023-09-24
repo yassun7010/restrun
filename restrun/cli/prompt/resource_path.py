0977fe20dce9d1fbbbcbf1e181fb15640d2f9ff1
yassun7010 <yassun7010@outlook.com>
2023-09-24 14:15:22 +0900

    Move: module position.

--- restrun/cli/prompt/resource_path.py
+++ restrun/cli/prompt/resource_path.py
@@ -1,6 +1,7 @@
-def prompt_resource_path(resource_path: str | None) -> str:
-    from rich.prompt import Prompt
+from rich.prompt import Prompt
 
+
+def prompt_resource_path(resource_path: str | None) -> str:
     while not resource_path:
         resource_path = Prompt.ask(
             '[dark_orange]Resource Path[/] like [green]"/ptes/{petId}"[/]',
